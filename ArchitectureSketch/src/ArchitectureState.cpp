1f37f9ef735a91bfad6a1a09f7678d4134a6a055
JeroenHoogers <hoogers.jeroen@gmail.com>
2018-03-05 15:13:07 +0100

    Added mass model evolution

--- ArchitectureSketch/src/ArchitectureState.cpp
+++ ArchitectureSketch/src/ArchitectureState.cpp
@@ -51,13 +51,87 @@ void ArchitectureState::setup()
 
 	ofSetLineWidth(1.5);
 
-	camera.setDistance(30);
+	camera.setDistance(40);
 	camera.disableRoll();
 
 	//post.init(ofGetWidth(), ofGetHeight());
 	//post.createPass<FxaaPass>();
 	////post.createPass<BloomPass>();
 	//post.createPass<SSAOPass>();
+
+	testModel.m1 = Mass(-8, 8, -6, 6, 2);
+	testModel.m2 = Mass(-16, -8, -5, 5, 1);
+	testModel.m3 = Mass(8, 16, -5, 5, 1);
+
+	// button events
+	generateNextGenButton.addListener(this, &ArchitectureState::generateButtonPressed);
+
+	//// major parameters (require genetic algorithm reset)
+	//mDimensions.addListener(this, &ArchitectureState::majorParameterChanged);
+	//mPopulationSize.addListener(this, &ArchitectureState::majorParameterChanged);
+
+	// minor parameters (do not require GA reset)
+	mMutationRate.addListener(this, &ArchitectureState::minorParameterChanged);
+	mMutationAmount.addListener(this, &ArchitectureState::minorParameterChanged);
+
+	// setup gui 
+	gui.setup("GA Settings");
+	//gui.add(mDimensions);
+	//gui.add(mPopulationSize);
+	gui.add(mMutationRate);
+	gui.add(mMutationAmount);
+	gui.add(generateNextGenButton.setup("Generate offspring"));
+
+	setupEvolution();
+}
+
+//--------------------------------------------------------------
+void ArchitectureState::setupEvolution()
+{
+	// setup genetic algorithm
+	geneticAlgorithm.setup(9, 15, mMutationRate.get(), mMutationAmount.get());
+
+	massModels.clear();
+
+	for (int i = 0; i < geneticAlgorithm.population.size(); i++)
+	{
+		massModels.push_back(convertGenotype(geneticAlgorithm.population[i]));
+	}
+}
+
+//--------------------------------------------------------------
+MassModel ArchitectureState::convertGenotype(Genotype genotype)
+{
+	// range -30, 30
+	float max = 25;
+	int h = 3;
+	MassModel mm;
+	
+	mm.m1 = Mass(
+		(genotype[0] - 0.5f) * max,
+		(genotype[1] - 0.5f) * max,
+		(genotype[2] - 0.5f) * max,
+		(genotype[3] - 0.5f) * max,
+		floorf(genotype[4] * h + 0.99f)
+	);
+
+	mm.m2 = Mass(
+		(genotype[5] - 0.5f) * max,
+		(genotype[6] - 0.5f) * max,
+		(genotype[7] - 0.5f) * max,
+		(genotype[8] - 0.5f) * max,
+		floorf(genotype[9] * h + 0.99f)
+	);
+
+	mm.m3 = Mass(
+		(genotype[10] - 0.5f) * max,
+		(genotype[11] - 0.5f) * max,
+		(genotype[12] - 0.5f) * max,
+		(genotype[13] - 0.5f) * max,
+		floorf(genotype[14] * h + 0.99f)
+	);
+
+	return mm;
 }
 
 //--------------------------------------------------------------
@@ -81,13 +155,13 @@ void ArchitectureState::draw()
 
 		ofPushMatrix();
 		{
-			ofTranslate(x, y);
+			//ofTranslate(x, y);
 			//ofScale(1.0f / tilew, 1.0f / tileh);
 
 			//ofRectangle rect = ofRectangle(0, 0, tilew, tileh);
 			ofRectangle rect = ofRectangle(x, y, tilew, tileh);
 			
-			drawTile(rect);
+			drawTile(rect, i);
 		}
 		ofPopMatrix();
 	}
@@ -109,15 +183,32 @@ void ArchitectureState::draw()
 
 	camera.begin();
 	camera.end();
+
+	// show generation
+	ofSetColor(10);
+	ofDrawBitmapStringHighlight("Generation: " + ofToString(geneticAlgorithm.currentGeneration), 15, 25);
+
+	gui.draw();
 }
 
-
-void ArchitectureState::drawTile(ofRectangle viewport)
+//--------------------------------------------------------------
+void ArchitectureState::drawTile(ofRectangle viewport, int index)
 {
+	// show selection
+	for (int i = 0; i < selectedIndices.size(); i++)
+	{
+		if (selectedIndices[i] == index)
+		{
+			ofFill();
+			ofSetColor(50, 180, 50, 80);
+			ofDrawRectangle(viewport);
+			break;
+		}
+	}
+
 	ofSetLineWidth(1.5);
 	ofEnableDepthTest();
 
-	//post.begin(camera);
 	light.setPosition(-camera.getGlobalPosition());
 	light.lookAt(ofVec3f(0, 0, 0));
 
@@ -125,42 +216,61 @@ void ArchitectureState::drawTile(ofRectangle viewport)
 
 	// draw outlines
 	ofNoFill();
-	ofSetColor(140);
-
-	ofPushMatrix();
-	{
-		ofTranslate(0, 6);
-		ofDrawBox(16, 12, 16);
-	}
-	ofPopMatrix();
-
-	ofPushMatrix();
-	{
-		ofTranslate(12, 3);
-		ofDrawBox(8, 6, 12);
-	}
-	ofPopMatrix();
+	//ofSetColor(140);
 
 	ofEnableLighting();
 	//phong.begin();
 
 	// draw shapes
-	ofSetColor(255);
+	ofSetColor(220);
 	ofFill();
 
-	ofPushMatrix();
-	{
-		ofTranslate(0, 6);
-		ofDrawBox(16, 12, 16);
-	}
-	ofPopMatrix();
+	// draw mass 1
+	//ofPushMatrix();
+	//{
+	ofPoint pos;
+	ofPoint sz;
 
-	ofPushMatrix();
-	{
-		ofTranslate(12, 3);
-		ofDrawBox(8, 6, 12);
-	}
-	ofPopMatrix();
+	// draw mass 1
+	pos = massModels[index].m1.getPoint();
+	sz = massModels[index].m1.getSize();
+
+	ofSetColor(180, 180, 80, 255);
+	ofDrawBox(pos, sz.x, sz.y, sz.z);
+
+	// draw mass 2
+	pos = massModels[index].m2.getPoint();
+	sz = massModels[index].m2.getSize();
+
+	ofSetColor(80, 80, 180, 255);
+	ofDrawBox(pos, sz.x, sz.y, sz.z);
+
+	// draw mass 3
+	pos = massModels[index].m3.getPoint();
+	sz = massModels[index].m3.getSize();
+
+	ofSetColor(80, 180, 80, 255);
+	ofDrawBox(pos, sz.x, sz.y, sz.z);
+
+
+	//}
+	//ofPopMatrix();
+
+	//ofPushMatrix();
+	//{
+	//	ofSetColor(80, 80, 180, 255);
+	//	ofTranslate(12, 3);
+	//	ofDrawBox(8, 6, 12);
+	//}
+	//ofPopMatrix();
+
+	//ofPushMatrix();
+	//{
+	//	ofSetColor(80, 180, 80, 255);
+	//	ofTranslate(-12, 3);
+	//	ofDrawBox(8, 6, 12);
+	//}
+	//ofPopMatrix();
 
 	ofDisableLighting();
 	//phong.end();
@@ -172,10 +282,9 @@ void ArchitectureState::drawTile(ofRectangle viewport)
 
 		ofSetColor(120);
 		//ofDrawPlane(0, 0, 50, 50);
-		ofDrawGridPlane(2, 16);
+		ofDrawGridPlane(2, 8);
 	}
 	ofPopMatrix();
-
 
 	ofDisableDepthTest();
 
@@ -184,6 +293,100 @@ void ArchitectureState::drawTile(ofRectangle viewport)
 
 	camera.end();
 }
+
+//void ArchitectureState::drawTile(ofRectangle viewport)
+//{
+//	ofSetLineWidth(1.5);
+//	ofEnableDepthTest();
+//
+//	//post.begin(camera);
+//	light.setPosition(-camera.getGlobalPosition());
+//	light.lookAt(ofVec3f(0, 0, 0));
+//
+//	camera.begin(viewport);
+//
+//	//// draw outlines
+//	ofNoFill();
+//	//ofSetColor(140);
+//
+//	ofPushMatrix();
+//	{
+//		ofSetColor(180 * 0.8f, 180 * 0.8f, 80 * 0.8f, 255);
+//		ofTranslate(0, 6);
+//		ofDrawBox(16, 12, 16);
+//	}
+//	ofPopMatrix();
+//
+//	ofPushMatrix();
+//	{
+//		ofSetColor(80 * 0.8f, 80 * 0.8f, 180 * 0.8f, 255);
+//		ofTranslate(12, 3);
+//		ofDrawBox(8, 6, 12);
+//	}
+//	ofPopMatrix();
+//
+//	ofPushMatrix();
+//	{
+//		ofSetColor(80 * 0.8f, 180 * 0.8f, 80 * 0.8f, 255);
+//		ofTranslate(-12, 3);
+//		ofDrawBox(8, 6, 12);
+//	}
+//	ofPopMatrix();
+//
+//	ofEnableLighting();
+//	//phong.begin();
+//
+//	// draw shapes
+//	ofSetColor(220);
+//	ofFill();
+//
+//	ofPushMatrix();
+//	{
+//		ofSetColor(180, 180, 80, 255);
+//		ofTranslate(0, 6);
+//		ofDrawBox(16, 12, 16);
+//	}
+//	ofPopMatrix();
+//
+//	ofPushMatrix();
+//	{
+//		ofSetColor(80, 80, 180, 255);
+//		ofTranslate(12, 3);
+//		ofDrawBox(8, 6, 12);
+//	}
+//	ofPopMatrix();
+//
+//	ofPushMatrix();
+//	{
+//		ofSetColor(80, 180, 80, 255);
+//		ofTranslate(-12, 3);
+//		ofDrawBox(8, 6, 12);
+//	}
+//	ofPopMatrix();
+//
+//	ofDisableLighting();
+//	//phong.end();
+//
+//	ofPushMatrix();
+//	{
+//		//ofRotateX(-90);
+//		ofRotateZ(90);
+//
+//		ofSetColor(120);
+//		//ofDrawPlane(0, 0, 50, 50);
+//		ofDrawGridPlane(2, 16);
+//	}
+//	ofPopMatrix();
+//
+//
+//	ofDisableDepthTest();
+//
+//	//ofDrawAxis(5);
+//	//post.end();
+//
+//	camera.end();
+//}
+
 
 //--------------------------------------------------------------
 void ArchitectureState::drawGUI()
@@ -231,7 +434,7 @@ void ArchitectureState::keyPressed(int key)
 	{
 		if (!camera.getOrtho())
 		{
-			camera.setScale(0.05, 0.05, 0.05);
+			camera.setScale(0.1, 0.1, 0.1);
 			camera.enableOrtho();
 		}
 		else
@@ -245,5 +448,95 @@ void ArchitectureState::keyPressed(int key)
 //--------------------------------------------------------------
 void ArchitectureState::mousePressed(int x, int y, int button)
 {
+	if (button == 0)
+		mousePos = ofPoint(x, y);
+}
 
+//--------------------------------------------------------------
+void ArchitectureState::mouseReleased(int x, int y, int button)
+{
+	if (button == 0)
+	{
+		// if mouse hasn't moved do selection
+		if (mousePos != ofPoint(x, y))
+			return;
+
+		// calculate tile dimensions
+		int tilew = (ofGetWidth() / tilesHorizontal);
+		int tileh = (ofGetHeight() / tilesVertical);
+
+		// calculate index based on mouse position
+		int index = (x / tilew) + (y / tileh) * 3;
+		bool removed = false;
+
+		if (index < geneticAlgorithm.populationSize)
+		{
+			for (int i = 0; i < selectedIndices.size(); i++)
+			{
+				// remove index
+				if (selectedIndices[i] == index)
+				{
+					selectedIndices.erase(selectedIndices.begin() + i);
+					removed = true;
+					break;
+				}
+			}
+
+			if (!removed)
+				selectedIndices.push_back(index);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ArchitectureState::generateButtonPressed()
+{
+	cout << "current generation: " << geneticAlgorithm.currentGeneration << endl;
+
+	// find selected indices
+	for (int i = 0; i < selectedIndices.size(); i++)
+	{
+		geneticAlgorithm.select(selectedIndices[i]);
+	}
+
+	if (selectedIndices.size() > 0)
+	{
+		// let the genetic algorithm generate offspring based on the selection
+		geneticAlgorithm.generateOffspring();
+	}
+	else
+	{
+		setupEvolution();
+	}
+
+	//// mark survivors from the last generation
+	//for (int i = 0; i < mSelectionRectangles.size(); i++)
+	//{
+	//	if (i < nrSelected)
+	//		mSelectionRectangles[i].prevSelected = true;
+	//	else
+	//		mSelectionRectangles[i].prevSelected = false;
+	//}
+
+	massModels.clear();
+
+	for (int i = 0; i < geneticAlgorithm.population.size(); i++)
+	{
+		massModels.push_back(convertGenotype(geneticAlgorithm.population[i]));
+	}
+
+	selectedIndices.clear();
+}
+
+//--------------------------------------------------------------
+void ArchitectureState::majorParameterChanged(int& val)
+{
+	setupEvolution();
+}
+
+//--------------------------------------------------------------
+void ArchitectureState::minorParameterChanged(float& val)
+{
+	geneticAlgorithm.mutationRate = mMutationRate.get();
+	geneticAlgorithm.mutationAmount = mMutationAmount.get();
 }
--- ArchitectureSketch/src/ArchitectureState.h
+++ ArchitectureSketch/src/ArchitectureState.h
@@ -4,10 +4,56 @@
 #include "ofxEasyCam.h"
 #include "ofFbo.h"
 #include "ofxPostProcessing.h"
+#include "GeneticAlgorithm.h"
 
 #define ArchitectureState_StateName "ArchitectureState"
 
 #pragma once
+struct Mass
+{
+	int x1;
+	int x2;
+	int z1;
+	int z2;
+	int floors; // 0,1,2
+
+	Mass()
+	{
+	};
+
+	Mass(int _x1, int _x2, int _z1, int _z2, int _floors)
+	{
+		x1 = _x1;
+		x2 = _x2;
+		z1 = _z1;
+		z2 = _z2;
+		floors = _floors;
+	};
+
+	ofPoint getPoint()
+	{
+		return ofPoint(
+			(x1 + x2) / 2.0f,
+			floors * 2,
+			(z1 + z2) / 2.0f);
+	};
+
+	ofPoint getSize()
+	{
+		return ofPoint(
+			fabsf(x1 - x2),
+			floors * 4,
+			fabsf(z1 - z2));
+	};
+};
+
+struct MassModel
+{
+	Mass m1;
+	Mass m2;
+	Mass m3;
+};
+
 class ArchitectureState : public itg::ofxState<SharedData>
 {
 private:
@@ -18,6 +64,23 @@ private:
 	int tilesHorizontal = 3;
 	int tilesVertical = 3;
 
+	ofPoint mousePos;
+
+	ofxPanel gui;
+
+	ofParameter<int> mDimensions = ofParameter<int>("Nr of params", 5, 1, 20);
+	ofParameter<int> mPopulationSize = ofParameter<int>("Population size", 20, 10, 40);
+	ofParameter<float> mMutationRate = ofParameter<float>("Mutation rate", 0.2, 0.0, 1.0);
+	ofParameter<float> mMutationAmount = ofParameter<float>("Mutation amount", 0.4, 0.0, 1.0);
+	ofxButton generateNextGenButton;
+
+	vector<MassModel> massModels;
+	MassModel testModel;
+
+	vector<int> selectedIndices;
+
+	GeneticAlgorithm geneticAlgorithm;
+
 public:
 	ArchitectureState();
 	~ArchitectureState();
@@ -25,15 +88,28 @@ public:
 	void stateEnter();
 	void stateExit();
 	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
 
 	void setup();
+	void setupEvolution();
+
 	void update();
 	void draw();
 	void drawGUI();
-	void drawTile(ofRectangle viewport);
+	void drawTile(ofRectangle viewport, int index);
+
+	MassModel convertGenotype(Genotype genotype);
 
 	string getName();
 	void keyPressed(int key);
+	
+
+	void toggleSelected(int index);
+
+	void generateButtonPressed();
+
+	void minorParameterChanged(float &val);
+	void majorParameterChanged(int &val);
 
 };
 
